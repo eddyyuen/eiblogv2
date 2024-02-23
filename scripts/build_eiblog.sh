32a42cc7513583beca20071669201b4e349627e8
hamjin <jinham@qq.com>
2024-02-23 19:50:07 +0800

    script: Update build_eiblog.sh
    Signed-off-by: hamjin <jinham@qq.com>

--- scripts/build_eiblog.sh
+++ scripts/build_eiblog.sh
@@ -2,9 +2,8 @@
 # prepare dir ./bin
 app=eiblog
 _arch="$(go env GOARCH)"
-_tag="custom"
 _os="linux"
 # tar platform
-_target="$app-$_tag.$_os-$_arch.tar.gz"
-GOOS=$_os GOARCH=$_arch go build -tags prod -ldflags '-extldflags "-static"' -o eiblog "./cmd/$app"
+_target="$app-$_os-$_arch.tar.gz"
+CGO_ENABLED=1 GOOS=$_os GOARCH=$_arch go build -x -v -tags prod -ldflags '-s -w -linkmode "external" -extldflags "-static-libgcc -s" -linkshared' -o eiblog "./cmd/$app"
 tar czf $_target conf website assets eiblog
